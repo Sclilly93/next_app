ca6d4e710b642f610fab89533ede9a6a6da74e05
Seth Lilly <sethlilly93@gmail.com>
2024-03-01 13:25:17 -0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Custom configurations go here
+  // For example:
+  // basePath: '/my-app',  // Set the base path for your application
+  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+  //   // Customize webpack configuration
+  //   return config;
+  // },
+};
 
 export default nextConfig;
